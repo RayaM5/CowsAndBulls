c8b54f7835332175b1808ffd0b92d1bc2a30f87f
RayaM5 <raya.metche@gmail.com>
2020-11-14 22:10:16 +0200

    Fixed some bugs

--- main.py
+++ main.py
@@ -11,35 +11,36 @@ def Makenum ():
         
 
 def game(numd):
-     count= 0
+    count= 0
 
     while True:
-            guess = [int(i) for i in str(input("Please guess 4-digit number: "))]
-            while True:
-                len(guess) < len(set(guess)):
-                guess. clear()
-                guess = [int(i) for i in str(input("Please guess 4-digit number: "))]
-               
-            if guess == listn:
-                print("You won.")
-                print("It took you "+str(count)+" guess(es).")
-                break
+        guess = [int(i) for i in str(input("Please guess 4-digit number: "))]
+#        while True:
+#            if len(guess) < len(set(guess)):
+#                guess. clear()
+#                guess = [int(i) for i in str(input("Please guess 4-digit number: "))]
+           
+        if guess == listn:
+            print("You won.")
+            print("It took you "+str(count)+" guess(es).")
+            break
+        
+        elif count >= 10:
+            print (str(listn))
+            print("Looser!!! (>10 attempts)")
+            break
             
-            elif count >= 10:
-                print (str(listn))
-                break
-                
-            else:
-                cow=0
-                bull=0
-                for x in range(0,numd):
-                    if guess[x]==listn[x]:
-                        bull += 1
-                    elif guess[x] in listn:
-                        cow += 1
+        else:
+            cow=0
+            bull=0
+            for x in range(0,numd):
+                if guess[x]==listn[x]:
+                    bull += 1
+                elif guess[x] in listn:
+                    cow += 1
 
-            print("Cows: "+str(cow)+" Bulls: "+str(bull))
-
+        print("Cows: "+str(cow)+" Bulls: "+str(bull))
+        count+=1
 
 
 Makenum()
